e2c9e6b34e0a17558e8136fd32999c86e1af14cf
Maria Pomogaeva <maria.pomogaeva1@gmail.com>
2023-01-02 19:36:08 +0700

    Изменения после проверки

--- source/skript.js
+++ source/skript.js
@@ -1,14 +1,14 @@
-let navMain = document.querySelector('.main__nav');
-let navToggle = document.querySelector('.main__nav__button-toggle');
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
 
-navMain.classList.remove('main__nav--nojs');
+navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main__nav--closed')) {
-    navMain.classList.remove('main__nav--closed');
-    navMain.classList.add('main__nav--opened');
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
   } else {
-    navMain.classList.add('main__nav--closed');
-    navMain.classList.remove('main__nav--opened');
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
   }
 });
